355ac4d02e6f4359e3799ceb85b5276a0ea867d9
Teemo Tebest <teemo.tebest@gmail.com>
2022-07-27 12:08:51 +0200

    Improved functionality.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client';
 
 import App from './jsx/App.jsx';
 
+const element = document.createElement('div');
+element.setAttribute('id', 'app-root-2022-train_for_trade_ii_angola');
+document.getElementsByClassName('col-sm-3')[0].prepend(element);
+
 const container = document.getElementById('app-root-2022-train_for_trade_ii_angola');
 const root = createRoot(container);
 root.render(<App />);
--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,4 +1,6 @@
-import React, { /* useState, */useEffect } from 'react';
+import React, {
+  useRef, useState, useEffect, useCallback
+} from 'react';
 import '../styles/styles.less';
 
 // https://github.com/joshwnj/react-visibility-sensor
@@ -16,23 +18,52 @@ const easingFn = (t, b, c, d) => {
   return b + c * (tc + -3 * ts + 3 * t);
 };
 
+const appID = '#app-root-2022-train_for_trade_ii_angola';
+
+let width = 0;
 function App() {
+  let hasStarted = false;
+  let barsDrawn = false;
+
+  const appRef = useRef(null);
+  const [barWidth, setBarWidth] = useState(0);
+
   useEffect(() => {
+    setBarWidth(Math.min(appRef.current.offsetWidth - 20 - 60), 300);
   }, []);
 
-  const showGenders = () => {
-    document.querySelector('.genders_container').style.opacity = 1;
-  };
-  const showTargets = () => {
-    document.querySelector('.targets_container').style.opacity = 1;
-    document.querySelector('.public .bar.goal').style.width = `${(500 / 1000) * 240}px`;
-    document.querySelector('.public .bar.current').style.width = `${(974 / 1000) * 240}px`;
+  useEffect(() => {
+    width = barWidth;
+  }, [barWidth]);
 
-    document.querySelector('.private .bar.goal').style.width = `${(150 / 800) * 240}px`;
-    document.querySelector('.private .bar.current').style.width = `${(792 / 800) * 240}px`;
+  const showTargets = () => {
+    document.querySelector(`${appID} .targets_container`).style.opacity = 1;
+    document.querySelector(`${appID} .public .bar.goal`).style.width = `${(500 / 1000) * width}px`;
+    document.querySelector(`${appID} .public .bar.current`).style.width = `${(974 / 1000) * width}px`;
+
+    document.querySelector(`${appID} .private .bar.goal`).style.width = `${(150 / 800) * width}px`;
+    document.querySelector(`${appID} .private .bar.current`).style.width = `${(792 / 800) * width}px`;
     setTimeout(() => {
-      document.querySelectorAll('.change').forEach(el => { el.style.opacity = 1; });
+      document.querySelectorAll(`${appID} .change`).forEach(el => { el.style.opacity = 1; });
     }, 2000);
+  };
+  const handleWindowResize = useCallback(() => {
+    setBarWidth(Math.min(appRef.current.offsetWidth - 20 - 60), 300);
+    if (barsDrawn === true) {
+      showTargets();
+    }
+  }, [barsDrawn]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [handleWindowResize]);
+
+  const showGenders = () => {
+    document.querySelector(`${appID} .genders_container`).style.opacity = 1;
   };
 
   const onCoutUpEnd = () => {
@@ -40,18 +71,23 @@ function App() {
       showGenders();
     }, 200);
     setTimeout(() => {
+      barsDrawn = true;
       showTargets();
     }, 1500);
   };
 
+  const onCoutUpStart = () => {
+    hasStarted = true;
+  };
+
   return (
-    <div className="app">
+    <div className="app" ref={appRef}>
       <div className="total_container">
         <div className="value">
           <VisibilitySensor>
             {({ isVisible }) => (
               <span style={{ height: 71, display: 'inline-block', width: 182 }}>
-                {isVisible ? <CountUp delay={1} end={2016} start={0} duration={3} useEasing easingFn={easingFn} onEnd={onCoutUpEnd} formattingFn={formatNr} /> : null}
+                {(isVisible && hasStarted === false) ? <CountUp delay={1} end={2016} start={0} duration={3} useEasing easingFn={easingFn} onEnd={() => onCoutUpEnd()} onStart={onCoutUpStart} formattingFn={formatNr} /> : null}
               </span>
             )}
           </VisibilitySensor>
@@ -71,17 +107,17 @@ function App() {
       <div className="targets_container">
         <div className="target_container public">
           <div className="title">Public sector</div>
-          <div>
+          <div className="value_bar_wrapper">
             <div className="label">Goal</div>
             <div className="value">
               <div className="bar goal" data-width="500" />
               500 people
             </div>
           </div>
-          <div>
+          <div className="value_bar_wrapper">
             <div className="label">Current</div>
             <div className="value">
-              <div className="bar current" data-width="500" />
+              <div className="bar current" data-width="974" />
               974 people
               <div className="change">+195%</div>
             </div>
@@ -89,17 +125,17 @@ function App() {
         </div>
         <div className="target_container private">
           <div className="title">Private sector</div>
-          <div>
+          <div className="value_bar_wrapper">
             <div className="label">Goal</div>
             <div className="value">
-              <div className="bar goal" data-width="500" />
+              <div className="bar goal" data-width="150" />
               150
             </div>
           </div>
-          <div>
+          <div className="value_bar_wrapper">
             <div className="label">Current</div>
             <div className="value">
-              <div className="bar current" data-width="500" />
+              <div className="bar current" data-width="792" />
               792
               <div className="change">+528%</div>
             </div>
