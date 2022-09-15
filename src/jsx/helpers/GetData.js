42c2c22d84a26fd2381c882081fefc0b4116543b
Teemo Tebest <teemo.tebest@gmail.com>
2022-09-15 11:19:57 +0200

    Added production url

--- src/jsx/helpers/GetData.js
+++ src/jsx/helpers/GetData.js
@@ -1,6 +1,6 @@
 import CSVtoJSON from './CSVtoJSON.js';
 
-export const getData = () => fetch('./assets/data/train_for_trade_ii_angola - data.csv')
+export const getData = () => fetch((window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-train_for_trade_ii_angola/assets/data/train_for_trade_ii_angola - data.csv' : './assets/data/train_for_trade_ii_angola - data.csv')
   .then(response => response.text())
   .then(body => CSVtoJSON(body));
 
