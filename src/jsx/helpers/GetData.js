61f025381e71d8506891388964e14528a9c10e22
Teemo Tebest <teemo.tebest@gmail.com>
2022-09-30 12:49:21 +0200

    Updated data fetching

--- src/jsx/helpers/GetData.js
+++ src/jsx/helpers/GetData.js
@@ -1,7 +1,12 @@
 import CSVtoJSON from './CSVtoJSON.js';
 
 export const getData = () => fetch((window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-train_for_trade_ii_angola/assets/data/train_for_trade_ii_angola - data.csv' : './assets/data/train_for_trade_ii_angola - data.csv')
-  .then(response => response.text())
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.text();
+  })
   .then(body => CSVtoJSON(body));
 
 export default getData;
