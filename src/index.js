f892b73e11eebbe65568f4298f6bb3d2a94e3bc4
Teemo Tebest <teemo.tebest@gmail.com>
2023-12-05 12:46:29 +0100

    Updated packages and made code more resistant to changes in the page

--- src/index.js
+++ src/index.js
@@ -8,16 +8,22 @@ import AngolaMap from './jsx/AngolaMap.jsx';
 
 const side_panel = document.createElement('div');
 side_panel.setAttribute('id', 'app-root-2022-train_for_trade_ii_angola_side_panel');
-document.getElementsByClassName('col-sm-3')[0].getElementsByClassName('labelClass')[0].prepend(side_panel);
+document.getElementsByClassName('col-sm-3')[0].getElementsByClassName('block-header')[0].prepend(side_panel);
 
 const container_side_panel = document.getElementById('app-root-2022-train_for_trade_ii_angola_side_panel');
-const root_side_panel = createRoot(container_side_panel);
-root_side_panel.render(<SidePanel />);
+if (container_side_panel) {
+  const root_side_panel = createRoot(container_side_panel);
+  root_side_panel.render(<SidePanel />);
+}
 
 const container_policies = document.getElementById('app-root-2022-train_for_trade_ii_angola_policies');
-const root_policies = createRoot(container_policies);
-root_policies.render(<Policies />);
+if (container_policies) {
+  const root_policies = createRoot(container_policies);
+  root_policies.render(<Policies />);
+}
 
 const container_angola_map = document.getElementById('app-root-2022-train_for_trade_ii_angola_map');
-const root_angola_map = createRoot(container_angola_map);
-root_angola_map.render(<AngolaMap />);
+if (container_angola_map) {
+  const root_angola_map = createRoot(container_angola_map);
+  root_angola_map.render(<AngolaMap />);
+}
