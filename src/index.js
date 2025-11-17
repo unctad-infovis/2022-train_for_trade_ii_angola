ff95246561409f28e8a40af0349acfb2d9ee31d7
Teemo Tebest <teemo.tebest@gmail.com>
2025-11-17 15:40:02 +0100

    Fixed

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import AngolaMap from './jsx/AngolaMap.jsx';
 
 const side_panel = document.createElement('div');
 side_panel.setAttribute('id', 'app-root-2022-train_for_trade_ii_angola_side_panel');
-document.getElementsByClassName('col-sm-3')[0].getElementsByClassName('block-header')[0].prepend(side_panel);
-
-const container_side_panel = document.getElementById('app-root-2022-train_for_trade_ii_angola_side_panel');
-if (container_side_panel) {
-  const root_side_panel = createRoot(container_side_panel);
-  root_side_panel.render(<SidePanel />);
+try {
+  document.getElementsByClassName('sidebar-content--light-blue')[0].prepend(side_panel);
+  const container_side_panel = document.getElementById('app-root-2022-train_for_trade_ii_angola_side_panel');
+  if (container_side_panel) {
+    const root_side_panel = createRoot(container_side_panel);
+    root_side_panel.render(<SidePanel />);
+  }
+} catch {
+  console.log('error');
 }
 
 const container_policies = document.getElementById('app-root-2022-train_for_trade_ii_angola_policies');
